d878a5988d76f0e0c310e00d2c19cf5c36f174f9
venunadh-08 <venunadhparimi8560@gmail.com>
2025-12-30 18:53:10 +0530

    Commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/algorithmst-26/', // Base URL for GitHub Pages
   server: {
     port: 3006,
   }
