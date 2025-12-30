9700ff92b239d702b3749f995292ad9d309b5b80
venunadh-08 <venunadhparimi8560@gmail.com>
2025-12-30 18:15:03 +0530

    Deployement

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/algorithmst-26/', // Base URL for GitHub Pages
   server: {
     port: 3006,
   }
